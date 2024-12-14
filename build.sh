b2314e350aa1886cc5e098114122cfd603e3ca10
Varun22240 <thisisvarungundemoni@gmail.com>
2024-12-14 11:37:41 -0500

    Update build script and workflow configuration

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ cross build --release --target x86_64-unknown-linux-gnu
 cross build --release --target aarch64-unknown-linux-gnu
 
 # Authenticate GitHub
-# The token will be provided as an environment variable
 echo $GITHUB_TOKEN | gh auth login --with-token
 
 # Upload Binary to the Repository
